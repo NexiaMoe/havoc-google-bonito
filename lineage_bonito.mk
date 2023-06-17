7089fec72ca814a0cb07f7150fa8be83e1c16aea
Michael Bestas <mkbestas@gmail.com>
2023-06-17 12:46:07 +0300

    Initial havoc bonito
    Change-Id: Ic1b39c98f59b4f152225a4679f440d5dc55d5ac4

--- lineage_bonito.mk
+++ lineage_bonito.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bonito/aosp_bonito.mk)
@@ -21,6 +21,24 @@ PRODUCT_NAME := lineage_bonito
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
+
+HAVOC_BUILD_TYPE := Unofficial
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_WIFI_EXT := true
+# TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+WITH_GAPPS := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.control_privapp_permissions=log
+
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT==bonito \
--- lineage_sargo.mk
+++ lineage_sargo.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bonito/aosp_sargo.mk)
@@ -21,6 +21,21 @@ PRODUCT_NAME := lineage_sargo
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2220
 TARGET_SCREEN_WIDTH := 1080
+
+HAVOC_BUILD_TYPE := Unofficial
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_WIFI_EXT := true
+# TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+WITH_GAPPS := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.control_privapp_permissions=log
+
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sargo \
